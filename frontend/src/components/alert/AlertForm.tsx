ef19865cbd45fc32aadf13dcd73c2ac6065e26bc
jiangshu <jsxyjs123@gmail.com>
2026-03-16 08:30:32 +0800

    fix: remove blank lines in description placeholder and increase textarea rows to show full content

--- frontend/src/components/alert/AlertForm.tsx
+++ frontend/src/components/alert/AlertForm.tsx
@@ -250,7 +250,7 @@ export const AlertForm: React.FC = () => {
                   ]}
                   style={{ marginBottom: 12 }}
                 >
-                  <TextArea placeholder={t('form.descriptionPlaceholder')} rows={4} maxLength={500} showCount />
+                  <TextArea placeholder={t('form.descriptionPlaceholder')} rows={8} maxLength={500} showCount />
                 </Form.Item>
 
                 <Alert message={t('tips.descriptionHint')} type="info" showIcon style={{ marginBottom: 0, fontSize: 12 }} />
@@ -404,7 +404,7 @@ export const AlertForm: React.FC = () => {
                 { min: 10, message: t('form.descriptionMinLength', { min: 10 }) }
               ]}
             >
-              <TextArea placeholder={t('form.descriptionPlaceholder')} rows={6} maxLength={500} showCount />
+              <TextArea placeholder={t('form.descriptionPlaceholder')} rows={9} maxLength={500} showCount />
             </Form.Item>
 
             <Alert message={t('tips.descriptionHint')} type="info" showIcon style={{ marginBottom: 16 }} />
